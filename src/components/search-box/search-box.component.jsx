d724f0e504f622e59f391dda8401a9a03f1dbb3f
adnan1aust <adnan1aust@users.noreply.github.com>
2022-10-17 14:32:05 +0600

    First Commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import './search-box.styles.css'
+ class SearchBox extends Component{
+     render(){
+        const { className, placeholder, onChnageHandler} = this.props;
+         return (
+            <input 
+            className={`${className} search-box`}
+            type='search'
+            placeholder={placeholder}
+            onChange={onChnageHandler}
+            />
+         )
+     }
+ }
+
+ export default SearchBox;
